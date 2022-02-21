f94f067e548a839d8678ac3e2097510022d63ef6
Aron Theunissen <atheunissen@sogyo.nl>
2022-02-21 16:20:04 +0100

    Nu met Client

--- client/src/Mancala/Play.tsx
+++ client/src/Mancala/Play.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { GameState } from "../gameState";
 import "./Play.css";
+import { StartGameOnServer } from "./StartGame";
 
 type PlayProps = {
     gameState: GameState;
@@ -8,10 +9,43 @@ type PlayProps = {
 }
 
 export function Play({ gameState, setGameState }: PlayProps) {
-    return (
-        <div>
-            <p>{gameState.players[0].name} vs {gameState.players[1].name}</p>
-            To do...
-        </div>
-    )
-}
+    async function tryToPlay(index:number) {
+            //check if player has turn then
+            try {
+                const response = await fetch('mancala/api/play', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ index:index })
+                });
+
+                if (response.ok) {
+                    const updatedGameState = await response.json();
+                    setGameState({gameState, ...updatedGameState});
+                } else {
+                    console.error(response.statusText);
+                }
+            } catch (error) {
+                console.error(error.toString());
+            }
+        }
+
+
+
+
+        return (
+            <div>
+                <p>{gameState.players[0].name} vs {gameState.players[1].name}</p>
+                <div>
+                    {gameState.players[0].pits.map((pit) => <button className="pit"   onClick={()=>tryToPlay(pit.index)}>{pit.nrOfStones}  </button> )}
+                </div>
+                <div>
+                    {gameState.players[1].pits.map((pit) => <button className="pit"   onClick={()=>tryToPlay(pit.index)}>{pit.nrOfStones}  </button> )}
+                </div>
+                <p> The winner is: {gameState.gameStatus.winner}!
+                </p>
+            </div>
+        )
+    }
